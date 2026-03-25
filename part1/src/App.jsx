b7086916ed875385f7c5d667681f2285dab37b56
Gonzalo Acosta <gonzaloacosta258@gmail.com>
2026-03-25 12:07:40 -0300

    Uso de props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,23 @@
-const App = () => {
-  console.log('Hello from component')
+const Hello = (props) => {
+  console.log(props)  
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+  )
+}
+
+const App = () => {
+  const name = 'Charlie'
+  const age = '10'
+  return (
+    <>
+      <h1>Greetings</h1>
+
+      <Hello name="Alice" age={30} />
+      <Hello name="Bob" age={25+10} />
+      <Hello name={name} age={age} />
+    </>
   )
 }
 
