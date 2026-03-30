bb9917d8091167b4d4516f61636b3f0aafcb1625
Gonzalo Acosta <gonzaloacosta258@gmail.com>
2026-03-30 00:46:54 -0300

    courseinfo 1.5 ahora si

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,27 +3,29 @@ import Content from './components/Content'
 import Total from './components/Total' // importamos los componentes que creamos en la carpeta components para usarlos en el componente padre (App.jsx) 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return ( // los props se pasan como atributos a los componentes hijos (Header, Content y Total) desde la función padre (App.jsx)
     <div>
-      <Header course={course} /> 
-      <Content parts={parts} />    
-      <Total parts={parts} />   
+      <Header course={course.name} /> 
+      <Content parts={course.parts} />    
+      <Total parts={course.parts} />   
     </div>
   ) 
 }
