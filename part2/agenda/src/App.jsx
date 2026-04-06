1a3ec93243443de8ef2e93faa2f1555a9c45fe25
Gonzalo Acosta <gonzaloacosta258@gmail.com>
2026-04-06 20:52:52 -0300

    exercise 2.8 done

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -2,22 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 
+      number: '3625 175546',
+      id: 1
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
  const addPerson =(event) => {
   event.preventDefault()
   const personObject = {
     name: newName,
+    number: newNumber,
     id: persons.length + 1,
   }
-  if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
+  if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) { // Verifica nombres duplicados sin importar mayúsculas/minúsculas
     alert(`${newName} is already added to phonebook`)
     return
   }
   setPersons(persons.concat(personObject))
   setNewName('')
+  setNewNumber('')
  }
 
   return (
@@ -28,13 +34,16 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
-          <li key={person.id}>{person.name}</li>
+          <li key={person.id}>{person.name}: {person.number}</li>
         ))}
       </ul>
     </div>
