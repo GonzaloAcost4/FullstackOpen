e21508b362a06aeb7b43a5d066dc28ff9043bd6c
Gonzalo Acosta <gonzaloacosta258@gmail.com>
2026-04-13 20:39:58 -0300

    agenda 2.11 done

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Components.jsx/Filter'
 import PersonForm from './Components.jsx/PersonForm'
 import Persons from './Components.jsx/Persons'
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => { // El hook useEffect se usa para obtener los datos de la lista de personas desde un servidor.
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, []) // El array vacío asegura que el efecto se ejecute solo una vez, después del primer renderizado.  
 
   const addPerson = (event) => {
     event.preventDefault()
